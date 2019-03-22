02796cd8d0d71a5d69bc327295e7d44958cbdde3
Matheus Pegoraro <matheuspegoraro@icloud.com>
2019-03-22 20:04:55 -0300

    Primeiras conexoes com o mysql

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Whollie Services' });
-});
-
-router.get("/api/whoollie-food/empresas", function (req, res, next) {
-
-  conn.query("SELECT * FROM tbEmpresas", (err, results) => {
-
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(results);
-    }
-
-  });
-
 }); 
 
 module.exports = router;
