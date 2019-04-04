93a3516d543e9a5133421f14da92c94488deaeb3
Matheus Pegoraro <matheuspegoraro@icloud.com>
2019-04-03 22:28:03 -0300

    Correções da tela de produtos e categorias

--- vendor/WHOOLLIEFOOD/php-classes/src/MODEL/Product.php
+++ vendor/WHOOLLIEFOOD/php-classes/src/MODEL/Product.php
@@ -8,8 +8,10 @@ class Product{
 
 	private $desName;
 	private $desNote;
+	private $desImagePath;
 	private $vlUnity;
 	private $qtStock;
+	private $isActive;
 	private $idCompany;
 	private $idProductCategory;
 
@@ -39,6 +41,74 @@ class Product{
 		$this->idProductCategory = $idProductCategory;
 	}
 
+	public function setIsActive($isActive) {
+		$this->isActive = $isActive;
+	}
+
+	public function setDesImagePath($files) {
+
+		if ($files["image"]["name"] == "") {
+			$this->desImagePath = "res/admin/img/sem_foto.png";
+			
+			echo json_encode([
+				'error' => false
+			]);
+
+			return;
+		}
+		
+		$target_dir = "res/uploads/products/";
+		$target_file = $target_dir . time() . "_" . basename($files["image"]["name"]);
+		$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+		
+		if(getimagesize($files["image"]["tmp_name"]) === false) {
+			echo json_encode([
+				'error' => true,
+				"message" => "Arquivo não é uma imagem válida!"
+			]);
+			exit;
+		}
+
+		if (file_exists($target_file)) {
+			echo json_encode([
+				'error' => true,
+				"message" => "Imagem já existente em nosso banco de dados!"
+			]);
+			exit;
+		}
+
+		if ($files["image"]["size"] > 5 * 1024 * 1024) {
+			echo json_encode([
+				'error' => true,
+				"message" => "Imagem muito grande. Insira uma imagem de até 5MB!"
+			]);
+			exit;
+		}
+
+		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+			echo json_encode([
+				'error' => true,
+				"message" => "Tipo de imagem incorreto, somente JPG, JPEG, PNG e GIF!"
+			]);
+			exit;
+		}
+
+		if (move_uploaded_file($files["image"]["tmp_name"], $target_file)) {
+			$this->desImagePath = $target_file;
+			echo json_encode([
+				'error' => false
+			]);
+			return;
+		} else {
+			echo json_encode([
+				'error' => true,
+				"message" => "Erro ao transferir imagem!"
+			]);
+			exit;
+		}
+
+	}
+
 	public function getDesName(){
 		return $this->desName;
 	}
@@ -63,6 +133,14 @@ class Product{
 		return $this->idCompany;
 	}
 
+	public function getIsActive(){
+		return $this->isActive;
+	}
+
+	public function getDesImagePath() {
+		return $this->desImagePath;
+	}
+
 	public function listAll(){
 
 		$sql = new Sql();
@@ -79,7 +157,7 @@ class Product{
             ':IDPRODUTO' => $idProduct
         ]));
 
-    }
+	}
 
     public function createProduct(){
 
@@ -87,19 +165,17 @@ class Product{
 		
 		if($this->getDesName() != "" && $this->getVlUnity() != "" && $this->getQtStock() != "" && $this->getIdCompany() != "" && $this->getIdProductCategory() != ""){
 
-		$sql->query("INSERT INTO tbProducts(desName, desNote, qtStock, vlUnity, idCompany, idProductCategory) 
-			VALUES (:DESNAME, :DESNOTE, :QTSTOCK, :VLUNITY, :IDCOMPANY, :IDPRODUCTCATEGORY)", [
-				":DESNAME"=>$this->getDesName(),
-				":DESNOTE"=>$this->getDesNote(),
-                ":QTSTOCK"=>$this->getQtStock(),
-                ":VLUNITY"=>$this->getVlUnity(),
-				":IDCOMPANY"=>$this->getIdCompany(),
-				":IDPRODUCTCATEGORY"=>$this->getIdProductCategory()
-			]);
-
-			echo "INSERT INTO tbProducts(desName, desNote, qtStock, vlUnity, idCompany, idProductCategory) 
-			VALUES ('".$this->getDesName()."', '".$this->getDesNote()."', '".$this->getQtStock()."', '".$this->getVlUnity()."', '".$this->getIdCompany()."', '".$this->getIdProductCategory()."')";
-
+			$sql->query("INSERT INTO tbProducts(desName, desNote, desImagePath, qtStock, isActive, vlUnity, idCompany, idProductCategory) 
+						 VALUES (:DESNAME, :DESNOTE, :DESIMAGEPATH, :QTSTOCK, :ISACTIVE, :VLUNITY, :IDCOMPANY, :IDPRODUCTCATEGORY)", [
+					":DESNAME"=>$this->getDesName(),
+					":DESNOTE"=>$this->getDesNote(),
+					":DESIMAGEPATH"=>$this->getDesImagePath(),
+					":QTSTOCK"=>$this->getQtStock(),
+					":VLUNITY"=>$this->getVlUnity(),
+					":ISACTIVE"=>$this->getIsActive(),
+					":IDCOMPANY"=>$this->getIdCompany(),
+					":IDPRODUCTCATEGORY"=>$this->getIdProductCategory()
+				]);
 		}
 
 	}
