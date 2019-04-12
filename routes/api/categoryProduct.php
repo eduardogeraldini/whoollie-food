8147825b63bab9708c5d1cc2b0294f5e8b88d642
Eduardo <eduardopr.geraldini@gmail.com>
2019-04-12 20:03:43 -0300

    implementação categorias produtos

--- routes/api/categoryProduct.php
+++ routes/api/categoryProduct.php
@@ -3,6 +3,40 @@
 use \WHOOLLIEFOOD\MODEL\ProductCategory;
 use \WHOOLLIEFOOD\MODEL\User;
 
+$app->post('/api/categories/products/update/{id}', function($request, $response, $args) {
+
+	User::verifyLogin();
+    $input = $request->getParsedBody();
+
+	$category = new ProductCategory();
+
+	$category->setDesName($input['desName']); 
+	$category->setIsActive($input['isActive']);
+
+	$category->updateProductCategory($args['id']);
+	
+});
+
+$app->post('/api/categories/products/delete/{id}', function($request, $response, $args) {
+
+	User::verifyLogin();
+
+	$category = new ProductCategory();
+	 
+	$category->deleteProductCategory($args['id']);
+	
+});
+
+$app->get('/api/categories/products/{id}', function($request, $response, $args) {
+
+	User::verifyLogin();
+
+	$category = new ProductCategory();
+
+	echo $category->returnProductCategoryById($args['id']);
+	
+});
+
 $app->get('/api/categories/products', function($request, $response, $args) {
 
 	User::verifyLogin();
@@ -23,6 +57,7 @@ $app->post('/api/categories/products', function($request, $response, $args) {
 	$category = new ProductCategory();
 
 	$category->setDesName($input['desName']);
+	$category->setIsActive($input['isActive']);
 
 	$category->createProductCategory();
 	
--- vendor/WHOOLLIEFOOD/php-classes/src/MODEL/ProductCategory.php
+++ vendor/WHOOLLIEFOOD/php-classes/src/MODEL/ProductCategory.php
@@ -8,6 +8,7 @@ class ProductCategory{
 
     private $desName;
     private $idCompany;
+    private $isActive;
 
     public function __construct(){
 
@@ -19,13 +20,21 @@ class ProductCategory{
         $this->desName = $desName;
     }
 
+    public function setIsActive($isActive){
+        $this->isActive = $isActive;
+    }
+
     public function getDesName(){
         return $this->desName;
     }
 
     public function getIdCompany(){
 		return $this->idCompany;
-	}
+    }
+    
+    public function getIsActive(){
+        return $this->isActive;
+    }
 
     public function createProductCategory(){
 
@@ -33,13 +42,52 @@ class ProductCategory{
 
         if($this->getDesName() != ""){
 
-            $sql->query("INSERT INTO tbProductsCategories(idCompany, desName) 
-			VALUES (:IDCOMPANY, :DESNAME)", [
+            $sql->query("INSERT INTO tbProductsCategories(idCompany, desName, isActive) 
+			VALUES (:IDCOMPANY, :DESNAME, :ISACTIVE)", [
 				":IDCOMPANY"=>$this->getIdCompany(),
-				":DESNAME"=>$this->getDesName(),
+                ":DESNAME"=>$this->getDesName(),
+                ":ISACTIVE"=>$this->getIsActive()
             ]);
 
         }
+       
+    }
+
+    public function updateProductCategory($idProductCategory){
+
+        $sql = new Sql();
+
+        if($this->getDesName() != ""){
+
+            $sql->query("UPDATE tbProductsCategories SET desName = :DESNAME, isActive = :ISACTIVE WHERE idProductCategory = :IDPRODUCTCATEGORY", [
+                ":DESNAME"=>$this->getDesName(),
+                ":ISACTIVE"=>$this->getIsActive(),
+                ":IDPRODUCTCATEGORY"=> $idProductCategory
+            
+            ]);
+
+        }
+       
+    }
+
+    public function deleteProductCategory($idProductCategory){
+
+        $sql = new Sql();
+
+        $sql->query("UPDATE tbProductsCategories SET isDeleted = :ISDELETED WHERE idProductCategory = :IDPRODUCTCATEGORY", [
+            ":ISDELETED"=> 1,
+            ":IDPRODUCTCATEGORY"=> $idProductCategory
+        ]);
+       
+    }
+
+    public function returnProductCategoryById($idProductCategory){
+
+        $sql = new Sql();
+
+        return json_encode($sql->select("SELECT * FROM tbProductsCategories WHERE idProductCategory = :IDPRODUCTCATEGORY",[
+            ":IDPRODUCTCATEGORY" => $idProductCategory,
+        ]));
        
     }
 
@@ -47,7 +95,10 @@ class ProductCategory{
 
         $sql = new Sql();
 
-        return json_encode($sql->select("SELECT * FROM tbProductsCategories"));
+        return json_encode($sql->select("SELECT * FROM tbProductsCategories WHERE idCompany = :IDCOMPANY AND isDeleted = :ISDELETED", [
+            ':IDCOMPANY' => $this->getIdCompany(),
+            ':ISDELETED' => 0
+        ]));
 
     }
 
