c6a2d14e435501740a2130b13b4a5b86bba3f81f
Eduardo <eduardopr.geraldini@gmail.com>
2019-04-13 18:10:50 -0300

    iniciando design monitor de preparos

---
+++ routes/dashboard/monitor.php
@@ -0,0 +1,12 @@
+<?php 
+
+use \WHOOLLIEFOOD\MODEL\User;
+
+$app->get('/monitor', function ($request, $response, $args) {
+	
+	User::verifyLogin(false,false);
+	renderPage("monitor", "monitor", true);
+
+});
+
+?>
