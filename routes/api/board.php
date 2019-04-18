cb46a7b39aab67ef235517c91311434e59419986
ElderSam <samuel99_silva@hotmail.com>
2019-04-18 00:26:14 -0300

    implementando lista de mesas

--- routes/api/board.php
+++ routes/api/board.php
@@ -3,6 +3,38 @@
 use \WHOOLLIEFOOD\MODEL\User;
 use \WHOOLLIEFOOD\MODEL\Board;
 
+// ---------------- DELETA ------------------------
+$app->post('/api/board/delete/{id}', function($request, $response, $args) {
+	
+	User::verifyLogin();
+
+	$board = new Board();
+	$board->setIsDeleted(1);  
+	$board->deleteBoard($args['id']);
+	
+	
+});
+
+// ---------------- EDITA ------------------------
+$app->post('/api/board/update/{id}', function($request, $response, $args) {
+	
+	User::verifyLogin();
+
+	$input = $request->getParsedBody();
+
+	
+	$board = new Board();
+
+	$board->setVlBoard($input['vlBoard']);
+	$board->setQtPlaces($input['qtPlaces']);
+	//$board->setIsActive($input['isActive']);
+
+	$board->editBoard($args['id']);
+	
+});
+
+
+// -------------------- CRIA MESA --------------------------------------------
 $app->post('/api/boards', function($request, $response, $args) {
 
     User::verifyLogin();
@@ -13,16 +45,19 @@ $app->post('/api/boards', function($request, $response, $args) {
     
     $board->setVlBoard($input["vlBoard"]);
 	$board->setQtPlaces($input["qtPlaces"]);
+	//$board->setIsActive($input['isActive']);
 
 
-    $board->createBoard();
+    echo $board->createBoard();
 	
 });
 
-
+// ------------------------------- LISTA MESAS -----------------------------------
 $app->get('/api/boards', function($request, $response, $args) {
 
     User::verifyLogin();
+
+    $board = new Board();
 
     echo $board->listAllBoards();
 	
--- vendor/WHOOLLIEFOOD/php-classes/src/MODEL/Board.php
+++ vendor/WHOOLLIEFOOD/php-classes/src/MODEL/Board.php
@@ -1,91 +1,129 @@
 <?php
-
 namespace WHOOLLIEFOOD\MODEL;
-
 use \WHOOLLIEFOOD\DB\Sql;
-
 class Board {
-
     private $idBoard;
     private $idCompany;
 	private $vlBoard;
 	private $qtPlaces;
-	
 	private $isActive;
-
+	private $isDeleted;
+	
     public function __construct(){
 		$this->idCompany = $_SESSION['User']['idCompany'];
 	}
-
 	public function getIdBoard() {
 		return $this->idBoard;
 	}
-
 	public function setIdBoard($idBoard) {
 		$this->idBoard = $idBoard;
 	}
-
 	public function getIdCompany() {
 		return $this->idCompany;
 	}
-
 	public function setIdCompany($idCompany) {
 		$this->idCompany = $idCompany;
 	}
-
 	public function getVlBoard() {
 		return $this->vlBoard;
 	}
-
 	public function setVlBoard($vlBoard) {
 		$this->vlBoard = $vlBoard;
 	}
-
 	public function getQtPlaces() {
 		return $this->qtPlaces;
 	}
-
 	public function setQtPlaces($qtPlaces) {
 		$this->qtPlaces = $qtPlaces;
 	}
-
-
 	public function getIsActive() {
 		return $this->isActive;
 	}
-
 	public function setIsActive($isActive) {
 		$this->isActive = $isActive;
-    }
+	}
+	public function getIsDeleted() {
+		return $this->isDeleted;
+	}
+	public function setIsDeleted($isDeleted) {
+		$this->isDeleted = $isDeleted;
+	}
+
+
     
     public function createBoard() {
-
         $sql = new Sql();
         
-		$idBoard = $sql->query("INSERT INTO tbBoards(idCompany, vlBoard, qtPlaces) 
-						    VALUES (:IDCOMPANY, :VLBOARD, :QTPLACES)", [
-                            ":IDCOMPANY"=>$this->getIdCompany(),
-                            ":VLBOARD"=>$this->getVlBoard(),
-                            ":QTPLACES"=>$this->getQtPlaces()
-                           
-							]);
+		$idBoard = $sql->query("
+					INSERT INTO tbBoards(idCompany, vlBoard, qtPlaces) 
+					VALUES (:IDCOMPANY, :VLBOARD, :QTPLACES)", [
+					":IDCOMPANY"=>$this->getIdCompany(),
+					":VLBOARD"=>$this->getVlBoard(),
+					":QTPLACES"=>$this->getQtPlaces()
+					
+					]);
 
-       $this->setIdBoard($idBoard);
-
+	   $this->setIdBoard($idBoard);
+	   
+	   	if ($idBoard > 0) {
+			return json_encode([
+				'error' => false
+			]);
+	   	} else {
+			return json_encode([
+				'error' => true,
+				'message' => 'Erro ao cadastrar a mesa!',
+			]);
+	   }
 		
     }
     
     public function listAllBoards() {
 
-        $sql = new Sql();
-
-        return json_encode($sql->select("SELECT *
-                             FROM tbBoards
-                             ORDER BY vlBoard"));
-
-    }
-
-
+		$sql = new Sql();
+		
+        return json_encode($sql->select("
+		                     SELECT *
+							 FROM tbBoards
+							 WHERE 
+							 isDeleted = :ISDELETED AND
+							 idCompany = :IDCOMPANY
+                             ORDER BY vlBoard", [
+								":ISDELETED" => 0,
+								":IDCOMPANY" => $this->getIdCompany()
+							]));
+	}
+	
+	public function editBoard($id) {
+		$sql = new Sql();
+		
+		if($this->getVlBoard() != "" && $this->getQtPlaces() != "" && $this->getIdCompany() != "" ){
+	
+			$sql->query("UPDATE tbBoards SET
+							vlBoard = :VLBOARD, 
+							qtPlaces = :QTPLACES
+					
+						 WHERE
+							 idBoard = :IDBOARD", [
+					":VLBOARD"=>$this->getVlBoard(),
+					":QTPLACES"=>$this->getQtPlaces(),
+					":IDBOARD"=>$id
+				]);
+		}
+	
+	}
+	
+	public function deleteBoard($idBoard){
+	
+		$sql = new Sql();
+	
+		return $sql->query("UPDATE tbBoards SET isDeleted = :ISDELETED
+				WHERE idCompany = :IDCOMPANY AND idBoard = :IDBOARD", [
+				":ISDELETED"=>$this->getIsDeleted(),
+				":IDCOMPANY"=>$this->getIdCompany(),
+				":IDBOARD"=>$idBoard                       
+			]);
+	
+	} 
 }
-
 ?>
