31311a1534caf6b191c6730cae0f1bcf8a9a48fb
Matheus Pegoraro <matheuspegoraro@icloud.com>
2019-04-19 12:28:40 -0300

    Corrigin sistema

--- vendor/WHOOLLIEFOOD/php-classes/src/DB/Sql.php
+++ vendor/WHOOLLIEFOOD/php-classes/src/DB/Sql.php
@@ -29,7 +29,10 @@ class Sql {
 		$this->conn = new \PDO(
 			"mysql:dbname=".$this->DBNAME.";host=".$this->HOSTNAME, 
 			$this->USERNAME,
-			$this->PASSWORD
+			$this->PASSWORD, [
+				\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+    			\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+			]
 		);
 
 	}
