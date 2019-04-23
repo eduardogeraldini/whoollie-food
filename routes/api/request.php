4072ce1e0d8c21732eeffdc820271e7a7fcb393f
Eduardo <eduardopr.geraldini@gmail.com>
2019-04-23 11:33:53 -0300

    correção array cart

--- routes/api/request.php
+++ routes/api/request.php
@@ -29,30 +29,6 @@ $app->get('/api/products/request/{id}', function($request, $response, $args) {
     
 });
 
-$app->get('/api/requests/orders/{id}', function($request, $response, $args) {
-
-	User::verifyLogin();
-    
-    $request = new Request();
-
-    $request->setIdOrder($args['id']);
-    
-	echo $request->listRequestsByOrders();
-    
-});
-
-$app->get('/api/requests/products/{id}', function($request, $response, $args) {
-
-	User::verifyLogin();
-    
-    $request = new Request();
-
-    $request->setIdRequest($args['id']);
-    
-	echo $request->listProductsInRequest();
-    
-});
-
 $app->get('/api/requests', function($request, $response, $args) {
 
 	if (!Device::verifyLogin()["login"]) {
@@ -77,5 +53,29 @@ $app->post('/api/request', function($request, $response, $args) {
     $request->openNewRequest();
     
 });
+
+$app->get('/api/requests/orders/{id}', function($request, $response, $args) {
+
+	User::verifyLogin();
+    
+    $request = new Request();
+
+    $request->setIdOrder($args['id']);
+    
+	echo $request->listRequestsByOrders();
+    
+});
+
+/*$app->get('/api/requests/products/{id}', function($request, $response, $args) {
+
+	User::verifyLogin();
+    
+    $request = new Request();
+
+    $request->setIdRequest($args['id']);
+    
+	echo $request->listProductsInRequest();
+    
+});*/
 
 ?>
