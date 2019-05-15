0f375636c72119b89f2f5b3239590ff2766b9d3b
Matheus Pegoraro <matheuspegoraro@icloud.com>
2019-05-15 19:46:22 -0300

    Corrigindo função ringbell

--- vendor/WHOOLLIEFOOD/php-classes/src/MODEL/Order.php
+++ vendor/WHOOLLIEFOOD/php-classes/src/MODEL/Order.php
@@ -208,9 +208,9 @@ class Order {
     public function ringBell() {
 
         $board = new Board();
-        $desName = $board->listBoardById($_SESSION["Device"]["idBoard"])["vlBoard"];
+        $vlBoard = json_decode($board->listBoardById((int) $_SESSION["Device"]["idBoard"]))["vlBoard"];
 
-        $data['message'] = 'O cliente '.$_SESSION["Order"]["desName"].' da mesa ['.$desName.'] está solicitando o atendimento !\n';
+        $data['message'] = 'O cliente '.$_SESSION["Order"]["desName"].' da mesa ['.$vlBoard.'] está solicitando o atendimento !\n';
         $this->pusher->trigger($this->getDesChannel(), "ringbell", $data);
 
     }
