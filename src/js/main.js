3aff36fdbbfd52e36ce58b725b8f7db4ce5176cc
Matheus Pegoraro <matheuspegoraro@icloud.com>
2019-05-15 21:00:38 -0300

    Corrigindo função ringbell

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ $(function () {
         "debug": false,
         "newestOnTop": false,
         "progressBar": true,
-        "positionClass": "toast-bottom-right",
+        "positionClass": "toast-top-right",
         "preventDuplicates": false,
         "onclick": null,
         "showDuration": "300",
