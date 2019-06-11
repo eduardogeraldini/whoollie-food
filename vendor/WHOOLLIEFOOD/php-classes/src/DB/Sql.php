fff276a0251553658c4770a4dfcbd02f9a56c2a4
Matheus Pegoraro <matheuspegoraro@icloud.com>
2019-06-11 20:19:09 -0300

    Corrigindo a hora do mysql

--- vendor/WHOOLLIEFOOD/php-classes/src/DB/Sql.php
+++ vendor/WHOOLLIEFOOD/php-classes/src/DB/Sql.php
@@ -36,7 +36,7 @@ class Sql {
 			[\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, \PDO::ATTR_PERSISTENT => true, \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8' COLLATE 'utf8_unicode_ci'"]
 		);
 
-		$this->exec("SET time_zone = 'America/Sao_Paulo'");
+		//$this->exec("SET time_zone = 'America/Sao_Paulo'");
 
 	}
 
