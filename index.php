1cce12fb23bb11b90d708a104a65cd3b37e4cc21
Matheus Pegoraro <matheuspegoraro@icloud.com>
2019-06-11 20:01:58 -0300

    Correção na hora da finalização de uma comanda

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 session_start();
 
+setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+date_default_timezone_set('America/Sao_Paulo');
+
 require_once("functions.php");
 
 initialize(["api","dashboard"]);
--- vendor/WHOOLLIEFOOD/php-classes/src/DB/Sql.php
+++ vendor/WHOOLLIEFOOD/php-classes/src/DB/Sql.php
@@ -2,9 +2,6 @@
 
 namespace WHOOLLIEFOOD\DB;
 
-setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
-date_default_timezone_set('America/Sao_Paulo');
-
 class Sql {
 
 	private $HOSTNAME = "";
