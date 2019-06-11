3f36a0e3ca174f227767b9393563fc38ce1b7dd2
Matheus Pegoraro <matheuspegoraro@icloud.com>
2019-06-11 20:12:57 -0300

    Correção na hora da finalização de uma comanda

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 
 session_start();
 
-setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
-date_default_timezone_set('America/Sao_Paulo');
-
 require_once("functions.php");
 
 initialize(["api","dashboard"]);
